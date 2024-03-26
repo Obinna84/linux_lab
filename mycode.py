eb79d574532785c3b850d2343b1c8e8068854781
Obinna84 <68216674+Obinna84@users.noreply.github.com>
2024-03-26 10:46:02 -0400

    Create mycode.py

---
+++ mycode.py
@@ -0,0 +1,29 @@
+import sys
+
+def next_char(c,shift):
+  c = c.upper()
+  start = ord('A')
+  if c.isalpha():
+    if ord(c) - 26 >= start:
+      return chr((ord(c) - 26) + shift)
+    return chr(ord(c) + shift)
+
+output = ""
+char_counter = 0
+spaces = 0
+amount_shifted = int(sys.argv[1])
+
+for line in sys.stdin:
+  if line.rstrip() == "q":
+    break
+  for i in line:
+    if next_char(i,amount_shifted) != None:
+      char_counter+=1
+      output+=next_char(i,amount_shifted)
+      if char_counter % 5 == 0:
+        output += " "
+        spaces +=1
+        if spaces != 0 and spaces % 10 == 0:
+          output+="\n"
+      
+print(output)
